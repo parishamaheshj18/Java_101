e656f7d390dff0cbb86e706a37e3a8b725ca3a46
Parisha Joshi <parishajm@gmail.com>
2020-12-28 11:41:30 -0500

    Readfile

--- Java_UdemyTutorials/src/udemyPackage/ReadFile.java
+++ Java_UdemyTutorials/src/udemyPackage/ReadFile.java
@@ -1,5 +1,22 @@
 package udemyPackage;
 
-public class ReadFile {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class ReadFile {
+	public static void main(String[] args) throws FileNotFoundException {
+		String fileName = "text.docx";
+		File textfile = new File(fileName);
+		Scanner in = new Scanner(textfile);
+		int value = in.nextInt();
+		System.out.println(value);
+		
+		while(in.hasNextLine()) {
+			String line = in.nextLine();
+			System.out.println(line);
+		}
+		in.close();
+		
+	}
 }
