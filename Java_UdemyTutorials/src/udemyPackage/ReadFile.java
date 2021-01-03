b7f3420dd349b3c5ee9c941b7354c300ba25bc48
Parisha Joshi <parishajm@gmail.com>
2021-01-03 14:14:24 -0500

    file added

--- Java_UdemyTutorials/src/udemyPackage/ReadFile.java
+++ Java_UdemyTutorials/src/udemyPackage/ReadFile.java
@@ -6,15 +6,19 @@ import java.util.Scanner;
 
 public class ReadFile {
 	public static void main(String[] args) throws FileNotFoundException {
-		String fileName = "text.docx";
+		String fileName = "/Users/parisha/Desktop/text.txt";
+		//String fileName = "text.txt";
+		
 		File textfile = new File(fileName);
 		Scanner in = new Scanner(textfile);
 		int value = in.nextInt();
-		System.out.println(value);
-		
+		System.out.println("Read Value: "+value);
+		in.nextLine();
+		int count =2;
 		while(in.hasNextLine()) {
 			String line = in.nextLine();
-			System.out.println(line);
+			System.out.println(count+" : "+line);
+			count++;
 		}
 		in.close();
 		
